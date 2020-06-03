50b4b38441deec879294317c09795489a811d23e
Michail Smolinskiy <nntndfrk@gmail.com>
2020-06-03 21:32:28 +0300

    ch10-00-generics

--- ch10-00-generics/src/main.rs
+++ ch10-00-generics/src/main.rs
@@ -20,3 +20,19 @@ fn largest(list: &[i32]) -> i32 {
     }
     return largest;
 }
+
+#[test]
+fn test_largest() {
+    let number_list1 = vec![34, 50, 25, 100, 65];
+    let number_list2 = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    assert_eq!(
+        largest(&number_list1),
+        100
+    );
+
+    assert_eq!(
+        largest(&number_list2),
+        6000
+    )
+}
